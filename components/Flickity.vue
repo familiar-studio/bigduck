e9cfaba871271aedae0e60639e1f6436fe691bdb
Scott Ries <scottmries@gmail.com>
2017-06-19 14:43:55 -0400

    Add first-slide class and slide index.

--- components/Flickity.vue
+++ components/Flickity.vue
@@ -1,6 +1,9 @@
 <template>
-    <div :class="{'open':open}" @click="toggleSize()" class="gallery-wrapper">
-      <slot ></slot>
+  <div class="carousel" :class="{'is-expanded':open, 'first-slide':isFirstSlide}">
+      Slide {{index}} of {{length}}
+      <div class="carousel-cell" v-for="image in images"> <img :src="image.sizes.large" :alt="image.title" class="img-fluid" @click="toggleSize()">
+        <figcaption class="figure-caption">{{image.caption}}</figcaption>
+      </div>
     </div>
 </template>
 
@@ -12,7 +15,7 @@ if (process.BROWSER_BUILD) {
 }
 
 export default {
-  created () {},
+  props: ['images'],
   data () {
     return {
       open: false,
@@ -31,13 +34,39 @@ export default {
   },
   methods: {
     toggleSize () {
+      console.log('toggle size')
       this.open = !this.open
       this.flickity.resize()
+    }
+  },
+  computed: {
+    isFirstSlide () {
+      return this.index === 1
+    },
+    index () {
+      if (this.flickity) {
+        return this.flickity.selectedIndex + 1
+      }
+    },
+    length () {
+      if (this.flickity) {
+        return this.flickity.slides.length
+      }
     }
   }
 }
 </script>
 <style>
+.carousel.is-expanded { height: 100vh; }
+.carousel.is-expanded img {max-width: 100%; height: auto;}
+.carousel.is-expanded .carousel-cell {
+  background-size: cover;
+  background-position: center;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 
+}
 
 </style>
