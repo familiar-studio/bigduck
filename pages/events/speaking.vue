8c6c1bf0b4d9dc1e4c2aad8018dbef7ce0ecf586
Keith Mancuso <keithmancuso@gmail.com>
2017-06-20 18:53:38 -0400

    speaking engagement page image

--- pages/events/speaking.vue
+++ pages/events/speaking.vue
@@ -1,10 +1,15 @@
 <template>
-<div class="container bg-white ">
-  <article class="main">
-    <h1>{{this.title}}</h1>
-    <div v-if="pageContent" v-html="pageContent">
-    </div>
-  </article>
+<div>
+  <div v-if="featuredImage" class="img-hero" :style="{ backgroundImage: 'url(' + featuredImage + ')' }">
+    <figcaption class="figure-caption">Hardcoded for now</figcaption>
+  </div>
+  <div class="container" :class="{'overlap':featuredImage}">
+    <article class="main">
+      <h1>{{this.title}}</h1>
+      <div v-if="pageContent" v-html="pageContent">
+      </div>
+    </article>
+  </div>
 </div>
 </template>
 <script>
@@ -13,12 +18,14 @@
   export default {
     name: 'speaking',
     async asyncData ({store}) {
-      let response = await Axios.get(store.getters['hostname'] + 'wp/v2/pages?slug=interested-in-having-big-duck-speak-at-your-organization')
+      let response = await Axios.get(store.getters['hostname'] + 'wp/v2/pages?slug=speaking-engagements')
 
       if (response.data) {
         return {
           title: response.data[0].title.rendered,
-          pageContent: response.data[0].content.rendered
+          pageContent: response.data[0].content.rendered,
+          featuredImage: response.data[0].acf.featured_image,
+          data: response.data[0]
         }
       }
     },
