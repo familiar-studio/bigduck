3da7fe2a8d7469d721da6a1e09d19711319961ff
Scott Ries <scottmries@gmail.com>
2017-06-21 15:29:40 -0400

    Add ACF-Search plugin.

--- wordpress/wp-content/themes/big-duck/functions.php
+++ wordpress/wp-content/themes/big-duck/functions.php
@@ -14,7 +14,40 @@ if ( ! class_exists( 'Timber' ) ) {
 
 
 
+function bd_search_join( $join ) {
+	global $wpdb;
 
+	if ( is_search() ) {
+		$join .=' LEFT JOIN '.$wpdb->postmeta. ' ON ' . $wpdb->posts . '.ID = ' . $wpdb->postmeta . '.post_id';
+	}
+	return $join;
+}
+
+add_filter('posts_join', 'bd_search_join');
+
+function bd_search_where( $where ) {
+    global $wpdb;
+
+    if ( is_search() ) {
+        $where = preg_replace(
+            "/\(\s*".$wpdb->posts.".post_title\s+LIKE\s*(\'[^\']+\')\s*\)/",
+            "(".$wpdb->posts.".post_title LIKE $1) OR (".$wpdb->postmeta.".meta_value LIKE $1)", $where );
+    }
+
+    return $where;
+}
+add_filter( 'posts_where', 'bd_search_where' );
+
+
+function bd_search_distinct( $where ) {
+	global $wpdb;
+	if ( is_search() ) {
+		return "DISTINCT";
+	}
+	return $where;
+}
+
+add_filter('posts_distinct', 'bd_search_distinct');
 
 add_filter( 'pre_get_posts', 'bd_cpt_search' );
 /**
