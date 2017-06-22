492d8fbbe020cf3ccbc3b7ad03ce9faf11019dc2
Scott Ries <scottmries@gmail.com>
2017-06-22 11:00:33 -0400

    Insights slug issues and Share functionality #31

--- pages/insights/_slug.vue
+++ pages/insights/_slug.vue
@@ -6,7 +6,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-lg-1 hidden-md-down">
-          <share></share>
+          <Share></Share>
         </div>
         <div class="col-lg-10">
           <div class="container overlap">
@@ -27,7 +27,7 @@
 
               <h1 v-html="insight.title.rendered"></h1>
               <div class="badge badge-default mb-3">
-                  <img v-if="insight.author_headshot" :src="insight.author_headshot.sizes.thumbnail" class="round author-img mr-2">
+                  <img v-if="insight.author_headshot.data" :src="insight.author_headshot.data.sizes.thumbnail" class="round author-img mr-2">
                   <div v-if="!insight.acf.is_guest_author" v-html="insight.acf.author.display_name"></div>
                   <div v-if="insight.acf.is_guest_author && insight.acf.author" v-html="entry.acf.author.display_name"></div>
               </div>
@@ -41,7 +41,7 @@
                 </template>
               </div>
               <div class="hidden-lg-up mt-4">
-                <share></share>
+                <Share></Share>
               </div>
             </article>
 
@@ -56,11 +56,11 @@
             <div class="mb-5" v-if="author && author.acf ">
               <div class="author-bio">
                 <div class="media">
-                  <img class="round" :src="insight.author_headshot.sizes.thumbnail" alt="" />
+                  <img class="round" if="insight.author_headshot.data" :src="insight.author_headshot.data.sizes.thumbnail" alt="" />
                   <div class="media-body">
                     <h3>{{insight.acf.author.display_name}} {{insight.acf.author.user_lastname}} is
                         {{ prependIndefiniteArticle(author.acf.job_title) }} at Big Duck</h3>
-                    <router-link class="btn btn-primary" :to="{name: 'team-slug', params: { slug: insight.acf.author.user_nicename}}">More about {{insight.acf.author.user_firstname}}</router-link>
+                    <router-link class="btn btn-primary" :to="{name: 'about-slug', params: { slug: insight.acf.author.user_nicename}}">More about {{insight.acf.author.user_firstname}}</router-link>
                   </div>
                 </div>
               </div>
@@ -72,10 +72,7 @@
               <div class="row">
                 <div v-for="case_study in relatedCaseStudies" class="col-md-6">
                   <router-link :to="{name: 'work-slug', params: {slug: case_study.slug}}" :key="case_study.ID">
-                    <!-- {{caseStudiesById[case_study.ID].acf.hero_image.sizes.large}} -->
-                    <img :src="case_study.acf.hero_image.sizes.large" style="width:100%">
-                    <!-- <div v-if="caseStudiesById[case_study.ID].acf.hero_image">
-                    </div> -->
+                    <img v-if="case_study.acf.hero_image" :src="case_study.acf.hero_image.sizes.large" style="width:100%">
                     <div class="card two-up-card mx-4">
                       <div class="card-header" v-if="topics && types">
                         <div class="badge badge-default" v-for="topic in case_study.topic">
@@ -127,7 +124,7 @@
       let response = await Axios.get(store.getters['hostname'] + 'wp/v2/bd_insight?slug=' + params.slug)
       data.insight = response.data[0]
       if (data.insight.acf) {
-        data.relatedWorkIds = data.insight.acf.related_case_studies
+        data.relatedWorkIds = data.insight.acf.related_case_studies.map((caseStudy) => { return caseStudy.ID })
       }
       return data
     },
@@ -163,7 +160,7 @@
     created () {
       // get related case studies
       if (this.relatedWorkIds) {
-        Axios.get(this.hostname + 'wp/v2/bd_case_study', { params: { includes: this.relatedWorkIds } }).then((response) => {
+        Axios.get(this.hostname + 'wp/v2/bd_case_study', { params: { include: this.relatedWorkIds } }).then((response) => {
           this.relatedCaseStudies = response.data
         })
       }
