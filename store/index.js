b18d4b1385a1595fff5453ca59530797b2e81079
Keith Mancuso <keithmancuso@gmail.com>
2017-06-26 16:31:06 -0400

    styles cleanup

--- store/index.js
+++ store/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const state = () => ({
-  localHostname: "http://wordpress.bigduck.dev/wp-json/",
+  localHostname: "https://wordpress.bigduck.dev/wp-json/",
   remoteHostname: "http://bigduck-wordpress.familiar.studio/wp-json/",
-  bareLocalHostname: "http://wordpress.bigduck.dev",
+  bareLocalHostname: "https://wordpress.bigduck.dev",
   bareRemoteHostname: "http://bigduck-wordpress.familiar.studio",
   callouts: null,
   categories: null,
@@ -195,6 +195,7 @@ export const getters = {
     // }
 
     return state.bareRemoteHostname;
+    //return state.bareLocalHostname;
   },
   callouts: state => {
     return state.callouts;
