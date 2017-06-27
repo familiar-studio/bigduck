48dd2896163a166bde9e437fd56596a4762b518b
Scott Ries <scottmries@gmail.com>
2017-06-27 18:20:35 -0400

    Person detail #48

--- pages/about/_slug.vue
+++ pages/about/_slug.vue
@@ -8,15 +8,13 @@
         <div class="badge badge-default">
           <nuxt-link to="/about#team">Team</nuxt-link>
         </div>
+        <h1 class="mt-4 mb-1">{{member.name}}</h1>
         <div class="row">
           <div class="col-md-9">
-            <h1 class="mt-4 mb-1">{{member.name}}</h1>
-            <h4>{{member.job_title}}</h4>
-            <div class="mt-3" v-html="member.bio"></div>
-          </div>
-          <div class="col-md-3">
-            <div class="mt-5 social-handle">
-              <a v-if="member.twitter_handle" :href="'http://twitter.com/' + member.twitter_handle">
+            <h4></h4>
+              <h4>
+                {{member.job_title}}
+                <a v-if="member.twitter_handle" :href="'http://twitter.com/' + member.twitter_handle" class="ml-3">
                 <div class="badge badge-default">
                   <div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.444 4.834c-.814.363-1.5.375-2.228.016.938-.562.981-.957 1.32-2.019-.878.521-1.851.9-2.886 1.104-.827-.882-2.008-1.435-3.315-1.435-2.51 0-4.544 2.036-4.544 4.544 0 .356.04.703.117 1.036-3.776-.189-7.125-1.998-9.366-4.748-.391.671-.615 1.452-.615 2.285 0 1.577.803 2.967 2.021 3.782-.745-.024-1.445-.228-2.057-.568l-.001.057c0 2.202 1.566 4.038 3.646 4.456-.666.181-1.368.209-2.053.079.579 1.804 2.257 3.118 4.245 3.155-1.945 1.524-4.356 2.159-6.728 1.881 2.012 1.289 4.399 2.041 6.966 2.041 8.358 0 12.928-6.924 12.928-12.929l-.012-.588c.887-.64 1.953-1.237 2.562-2.149z"/></svg>
@@ -32,7 +30,12 @@
                   <div v-html="member.facebook_page"></div>
                 </div>
               </a>
-            </div>
+              </h4>
+            <div class="mt-3" v-html="member.bio"></div>
+
+          </div>
+          <div class="col-md-3">
+
             <aside class="mt-5">
               <h4>{{ member.additional_info_heading_1 }}</h4>
               <div v-html="member.additional_info_body_1" class="mb-4"></div>
