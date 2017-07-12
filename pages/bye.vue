09ea06e36338f8dc351e2ed0d3e469a0661c4107
Scott Ries <scottmries@gmail.com>
2017-07-12 11:16:43 -0400

    Add Bye page, closes #109

---
+++ pages/bye.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <div class="" v-html="data.content.rendered"></div>
+  </div>
+</template>
+<script>
+  import Axios from 'axios'
+
+  export default {
+
+    async asyncData({ store }) {
+      let response = await Axios.get(store.getters['hostname'] + 'wp/v2/pages?slug=bye')
+      var data = response.data[0]
+      return {
+        data: data
+      }
+    }
+  }
+</script>
