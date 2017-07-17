d117fa38882ba05fe9915a1732aa8d8e27407ca4
Scott Ries <scottmries@gmail.com>
2017-07-17 14:47:32 -0400

    Form loading spinner.

--- components/GravityForm.vue
+++ components/GravityForm.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    <transition name="fade" appear>
-      <form v-if="!submitted && visibleFields">
-  
+        <transition name="fade" appear mode="out-in">
+
+      <div class="" v-if="loading" key="loading" class="loading">
+        <div class="loader loader--style3" title="2">
+          <svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;" xml:space="preserve">
+          <path fill="#fff" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z">
+            <animateTransform attributeType="xml"
+              attributeName="transform"
+              type="rotate"
+              from="0 25 25"
+              to="360 25 25"
+              dur="0.6s"
+              repeatCount="indefinite"/>
+            </path>
+          </svg>
+        </div>
+      </div>
+
+      <form v-else-if="!submitted && visibleFields" key="form">
+
         <div v-for="field in visibleFields" class="form-group" :class="{'has-danger':errors.has(field.id.toString())}">
-  
+
           <label :for="field.id" v-if="field.type != 'hidden'">{{field.label}}</label>
-  
+
           <template v-if="field.type == 'select'">
             <select v-model="formData['input_'+field.id]" class="custom-select form-control">
               <option v-for="choice in field.choices" :value="choice.value">
@@ -14,7 +32,7 @@
               </option>
             </select>
           </template>
-  
+
           <template v-else-if="field.type == 'checkbox'">
             <div class="custom-controls-stacked">
               <label class="custom-control custom-checkbox" v-for="choice in field.choices">
@@ -33,48 +51,48 @@
               </label>
             </div>
           </template>
-  
+
           <template v-else-if="field.type == 'email'">
             <input v-model="formData['input_'+field.id]" type="email" :name="field.id" class="form-control" v-validate="{ rules: { required: field.isRequired, email: true } }" />
           </template>
-  
+
           <template v-else-if="field.type == 'number'">
             <input v-model="formData['input_'+field.id]" type="number" :name="field.id" class="form-control" v-validate="{ rules: { required: field.isRequired, numeric: true } }" />
           </template>
-  
+
           <template v-else-if="field.type == 'hidden'">
             <input v-model="formData['input_'+field.id]" :name="field.id" type="hidden" />
           </template>
-  
+
           <template v-else-if="field.type == 'textarea'">
             <textarea v-model="formData['input_'+field.id]" class="form-control" :name="field.id" v-validate="{ rules: { required: field.isRequired } }" />
           </template>
-  
+
           <template v-else>
             <input v-model="formData['input_'+field.id]" type="text" class="form-control" :name="field.id" v-validate="{ rules: { required: field.isRequired } }" />
           </template>
-  
+
           <div class="form-control-feedback" v-show="errors.has(field.id.toString())">{{ errors.first(field.id.toString()) }}</div>
-  
+
         </div>
-  
+
         <div v-for="field in hiddenFields">
           <input v-model="formData['input_'+field.id]" :name="field.id" type="hidden" />
         </div>
-  
+
         <button type="submit" @click.prevent="submitEntry()" class="btn" :class="'btn-' + btnType">Submit</button>
       </form>
-      <div v-else>
+      <div v-else :key="confirmation">
         <h2>{{confirmation}}</h2>
-  
+
       </div>
-    </transition>
+      </transition>
   </div>
 </template>
 
 <script>
-import CryptoJS from 'crypto-js'
 import axios from 'axios'
+import CryptoJS from 'crypto-js'
 import { mapGetters } from 'vuex'
 
 if (process.BROWSER_BUILD) {
@@ -92,7 +110,8 @@ export default {
       totalProfilingFields: 2,
       submitted: false,
       confirmation: 'Thanks, your the greatest!',
-      hiddenFields: []
+      hiddenFields: [],
+      loading: false
 
     }
   },
@@ -166,6 +185,7 @@ export default {
       return encodeURIComponent(base64)
     },
     async submitEntry() {
+      this.loading = true
       var signature = this.CalculateSig('entries', 'POST')
       localStorage.formData = JSON.stringify(this.formData)
       //this.formData['form_id'] = this.formId
@@ -209,6 +229,7 @@ export default {
         }
       }
 
+      this.loading = false
       this.$emit('submitted')
       this.submitted = true
     }
@@ -235,3 +256,9 @@ export default {
   }
 }
 </script>
+<style>
+.loading {
+  display: flex;
+  justify-content: center;
+}
+</style>
