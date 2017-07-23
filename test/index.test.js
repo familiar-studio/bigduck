eda6417767f1dd2df96505b84cbba17ed128aeee
Keith Mancuso <keithmancuso@gmail.com>
2017-07-23 04:58:22 -0400

    added some more tests

--- test/index.test.js
+++ test/index.test.js
@@ -39,6 +39,40 @@ test("Route / exits and render HTML with CSS applied", async t => {
   // t.is(window.getComputedStyle(element).color, "red");
 });
 
+test("Route /services exits and render HTML", async t => {
+  let context = {};
+  const { html } = await nuxt.renderRoute("/services", context);
+  t.true(html.includes("block-overlap"));
+  t.true(html.includes("testimonial"));
+});
+
+test("Route /work exits and render HTML", async t => {
+  let context = {};
+  const { html } = await nuxt.renderRoute("/work", context);
+  t.true(html.includes("block-work-featured"));
+  t.true(html.includes("bg-img"));
+});
+
+test("Route /insights exits and render HTML", async t => {
+  let context = {};
+  const { html } = await nuxt.renderRoute("/insights", context);
+  t.true(html.includes("block-overlap"));
+  t.true(html.includes("featured-image"));
+});
+
+test("Route /about exits and render HTML", async t => {
+  let context = {};
+  const { html } = await nuxt.renderRoute("/about", context);
+  t.true(html.includes("about"));
+  t.true(html.includes("our-clients"));
+});
+
+test("Route /contact exits and render HTML", async t => {
+  let context = {};
+  const { html } = await nuxt.renderRoute("/contact", context);
+  t.true(html.includes("form-light"));
+});
+
 // Close server and ask nuxt to stop listening to file changes
 test.after("Closing server and nuxt.js", t => {
   server.close();
