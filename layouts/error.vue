6df1f6f8cabeb550dfa86d123022f23ad92283d4
Keith Mancuso <keithmancuso@gmail.com>
2017-07-24 15:07:20 -0400

    redirects

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,15 +3,19 @@
     <h1 v-if="error.statusCode === 404">Oh no! This page doesn&rsquo;t&nbsp;exist. </h1>
     <h1 v-else>An error occurred</h1>
     <div class="ascii-duck">
-      <h1><span class="bill-wrapper"><span>></span></span>(-)__,</h1>
+      <h1>
+        <span class="bill-wrapper">
+          <span>></span>
+        </span>(-)__,</h1>
       <h1>(___/</h1>
     </div>
-
+  
     <p class="block-text">
-      You can <a href="#" @click="goback()">go back</a> to where you were, or
+      You can
+      <a href="#" @click="goback()">go back</a> to where you were, or
       <nuxt-link to="/">return home</nuxt-link> to find one that does.
     </p>
-
+  
   </div>
 </template>
 
@@ -20,7 +24,7 @@ export default {
   props: ['error'],
   layout: 'default', // you can set a custom layout for the error page
   methods: {
-    goback(){
+    goback() {
       this.$router.go(-1)
     }
   }
