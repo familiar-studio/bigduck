6c712650681290971c66e1ac311b19e45c89f89a
Keith Mancuso <keith@familiar-studio.com>
2017-07-27 19:18:51 -0400

    Add act on to header

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,8 @@ module.exports = {
         content: "@bigduck"
       }
     ],
-    link: [{ rel: "apple-touch-icon", sizes:"152x152", href:"/favicon.png" }]
+    link: [{ rel: "apple-touch-icon", sizes:"152x152", href:"/favicon.png" }],
+    script: [{ src:"http://hi.bigducknyc.com/cdnr/30/acton/bn/tracker/4852"}]
   },
   cache: false,
   transition: {
