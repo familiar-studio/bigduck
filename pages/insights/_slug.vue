f0ceb3a810d7d89415b0aa9d0613978edec9238a
Scott Ries <scottmries@gmail.com>
2017-07-27 10:23:47 -0400

    Display insight callout block image, closes #186

--- pages/insights/_slug.vue
+++ pages/insights/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="insight">
-  
+
     <div class="img-hero" v-if="insight && insight.acf.featured_image" :style=" { backgroundImage: 'url(' + insight.acf.featured_image + ')' }">
       <figcaption class="figure-caption">{{insight.acf.featured_image.caption}}</figcaption>
     </div>
@@ -37,7 +37,7 @@
                   {{ date }}
                 </div>
               </div>
-  
+
               <h1 v-html="insight.title.rendered">
               </h1>
               <div class="author-listing" v-if="insight.acf.author.length > 0">
@@ -48,7 +48,7 @@
                     <nuxt-link :to="'/about/' + author.user_nicename">{{author.display_name}}</nuxt-link>
                   </div>
                 </div>
-  
+
                 <div>
                   <div v-if="insight.acf.guest_author_name" class="badge badge-default mb-3">
                     <img :src="backupImages['author']" class="round author-img mr-2">
@@ -61,25 +61,25 @@
               <div v-if="!insight.acf.guest_author_name && insight.acf.author.length < 1" class="badge badge-default mb-3 author-no-img">
                 <span>Big Duck</span>
               </div>
-  
+
               <div v-for="block in insight.acf.body" :class="['block-' + block.acf_fc_layout]">
                 <div v-if="block.acf_fc_layout == 'text'" v-html="block.text"></div>
                 <template v-if="block.acf_fc_layout == 'callout'">
                   <div v-html="block.text">
                   </div>
-                  <img :src="block.image" alt="callout image" v-if="block.image" />
+                  <div><img :src="block.image.url" alt="callout image" v-if="block.image" style="width:100%;"/></div>
                 </template>
               </div>
-  
+
               <div class="hidden-lg-up mt-4">
                 <Share></Share>
               </div>
             </article>
             <div v-if="insight.acf.is_gated_content">
-  
+
               <div class="form-light">
                 <GravityForm v-if="!formFilled" :formId="7" :gatedContent="insight.id" :title="insight.title.rendered" :id="insight.id" @submitted="refreshContent()" cookiePrefix="insight-"></GravityForm>
-  
+
                 <div v-if="formFilled || contentRefreshed">
                   <transition name="fade" appear>
                     <div>
@@ -90,12 +90,12 @@
                     </div>
                   </transition>
                 </div>
-  
+
               </div>
             </div>
-  
+
             <article class="mb-5">
-  
+
               <div v-if="insight.acf.author.length > 0" v-for="(author, index) in insight.acf.author">
                 <div class="author-bio">
                   <div class="row">
@@ -112,12 +112,12 @@
                         More about {{author.user_firstname}}
                       </nuxt-link>
                     </div>
-  
+
                   </div>
                 </div>
               </div>
             </article>
-  
+
             <div class="mb-5" v-if="relatedCaseStudies">
               <h2>Related Case Studies</h2>
               <div class="row">
@@ -144,7 +144,7 @@
                 </div>
               </div>
             </div>
-  
+
             <div class="mb-5" v-if="relatedInsights">
               <h2>Related Insights</h2>
               <div v-if="relatedInsights">
