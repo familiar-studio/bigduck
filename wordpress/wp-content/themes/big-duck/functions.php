770dd4fe7f5320e80ce19ece220d4536075b7872
Keith Mancuso <keithmancuso@gmail.com>
2017-07-30 17:17:53 -0400

    clean up and working

--- wordpress/wp-content/themes/big-duck/functions.php
+++ wordpress/wp-content/themes/big-duck/functions.php
@@ -137,18 +137,33 @@ function cc_mime_types($mimes) {
 }
 
 add_filter( 'upload_mimes', 'cc_mime_types');
-add_theme_support( 'post-formats' );
-add_theme_support( 'post-thumbnails' );
-add_theme_support( 'menus' );
-add_action( 'init', array( $this, 'register_post_types' ) );
-add_action( 'init', array( $this, 'register_taxonomies' ) );
-add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_bootstrap' ) );
-add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_bundles'));
-add_action( 'rest_api_init', array( $this, 'add_rest_fields'));
-add_action( 'rest_api_init', array( $this, 'register_routes') );
-add_action( 'init', array( $this, 'add_rest_to_cpts'));
-add_action( 'init', array( $this, 'add_image_sizes'));
-add_action( 'init', array( $this, 'handle_preflight'));
+
+
+class StarterSite  {
+
+	function __construct() {
+		add_theme_support( 'post-formats' );
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'menus' );
+		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_action( 'init', array( $this, 'register_post_types' ) );
+		add_action( 'init', array( $this, 'register_taxonomies' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_bootstrap' ) );
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_bundles'));
+		add_action( 'rest_api_init', array( $this, 'add_rest_fields'));
+		add_action( 'rest_api_init', array( $this, 'register_routes') );
+		add_action( 'init', array( $this, 'add_rest_to_cpts'));
+		add_action( 'init', array( $this, 'add_image_sizes'));
+		
+		add_action( 'init', array( $this, 'handle_preflight'));
+
+
+			
+
+
+	}
+
+
 
 	function handle_preflight() {
 		// Set the domain that's allowed to make the API call.
@@ -165,18 +180,7 @@ add_action( 'init', array( $this, 'handle_preflight'));
 	}
 
 	function register_routes() {
-		register_rest_route( 'familiar/v1', '/widgets', array(
-			'methods' => 'GET',
-			'callback' => array($this, 'get_items')
-		));
-		register_rest_route( 'familiar/v1', '/widgets/(?P<id_base>[\w-]+)', array(
-			'methods' => 'GET',
-			'callback' => array($this, 'get_item')
-		));
-		register_rest_route( 'familiar/v1/', '/widgets/(?P<id_base>[\w-]+)/(?P<instance_id>[\w-]+)', array(
-			'methods' => 'GET',
-			'callback' => array($this, 'get_item_instance')
-		));
+	
 		register_rest_route( 'familiar/v1', '/featured-work', array(
 			'methods' => 'GET',
 			'callback' => array($this, 'featured_work')
@@ -221,6 +225,7 @@ add_action( 'init', array( $this, 'handle_preflight'));
 
 	}
 
+
 	function team($object) {
 		$users = get_users(array(
 			'fields' => 'all_with_meta'
@@ -253,7 +258,6 @@ add_action( 'init', array( $this, 'handle_preflight'));
 			// add name, email, and slug to $fields
 			$team_member = $fields;
 		}
-
 		return new WP_REST_Response($team_member);
 	}
 
@@ -937,6 +941,12 @@ add_action( 'init', array( $this, 'handle_preflight'));
 		return $text;
 	}
 
+	function add_to_twig( $twig ) {
+		/* this is where you can add your own functions to twig */
+		$twig->addExtension( new Twig_Extension_StringLoader() );
+		$twig->addFilter('myfoo', new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+		return $twig;
+	}
 
 	function get_events( $data ){
 		$events = get_posts(array(
@@ -972,3 +982,6 @@ add_action( 'init', array( $this, 'handle_preflight'));
 		return new WP_REST_Response(array( 'insights' => $insights, 'categories' => $categories), 200);
 	}
 
+}
+
+new StarterSite();
