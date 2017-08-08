3a533947ed5aa0b391cf42d5e67deb4defcbc469
Keith Mancuso <keithmancuso@gmail.com>
2017-08-07 21:55:49 -0400

    turn off ssl all together
    :(

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -32,8 +32,8 @@ if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != "on") {
 		define('SSL', true);
 }
 
-define('WP_HOME','https://wordpress.bigducknyc.com');
-define('WP_SITEURL','https://wordpress.bigducknyc.com');
+define('WP_HOME','http://wordpress.bigducknyc.com');
+define('WP_SITEURL','http://wordpress.bigducknyc.com');
 
 
 define('DB_NAME', 'bigduck_wordpress');
