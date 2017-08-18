1f91a5a84244b19bc80aea9c83917d4757222508
Scott Ries <scottmries@gmail.com>
2017-08-18 12:46:28 -0400

    Remove insights by post date sort.

--- wordpress/wp-content/themes/big-duck/functions.php
+++ wordpress/wp-content/themes/big-duck/functions.php
@@ -24,14 +24,14 @@ function bd_pre_get_posts( $query ) {
 	return $query;
 }
 
-function sort_insights_by_date( $query ) {
-	if ( isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'bd_insight' ) {
-		$query->set('orderby', 'date');
-	}
-}
-
-
-add_filter('pre_get_posts', 'sort_insights_by_date');
+// function sort_insights_by_date( $query ) {
+// 	if ( isset($query->query_vars['post_type']) && $query->query_vars['post_type'] == 'bd_insight' ) {
+// 		$query->set('orderby', 'date');
+// 	}
+// }
+//
+//
+// add_filter('pre_get_posts', 'sort_insights_by_date');
 
 
 add_filter('pre_get_posts', 'bd_pre_get_posts');
