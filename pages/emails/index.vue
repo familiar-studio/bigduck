5ac0fd5a613afd633a8fdab9d0704cc117e5ad01
Keith Mancuso <keithmancuso@gmail.com>
2017-09-10 22:47:48 -0400

    basic emails setup

---
+++ pages/emails/index.vue
@@ -0,0 +1,137 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+
+<head>
+  <link rel="stylesheet" type="text/css" href="/../../../public/dist/emails/css/app.css">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title></title>
+  <!-- <style> -->
+</head>
+
+<body>
+  <span class="preheader"></span>
+
+  <table class="body">
+    <tr>
+      <td class="center" align="center" valign="top">
+        <center data-parsed="">
+           <table align="center" class="container float-center"><tbody><tr><td>
+  <table class="row logo logo-white bg-secondary"><tbody><tr>
+    <th class="small-12 large-12 columns first last"><table><tr><th>
+      <table class="spacer"><tbody><tr><td height="20px" style="font-size:20px;line-height:20px;">&#xA0;</td></tr></tbody></table> 
+      <a href="https://www.housingworks.org/">
+      <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/a6929ac8-16df-496b-8d2c-9022053129dc.png">
+      </a>
+      <table class="spacer"><tbody><tr><td height="10px" style="font-size:10px;line-height:10px;">&#xA0;</td></tr></tbody></table> 
+    </th>
+<th class="expander"></th></tr></table></th>
+  </tr></tbody></table>
+  <table class="wrapper bg-secondary" align="center"><tr><td class="wrapper-inner">
+    <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/6e524fe5-f6be-49ce-b35a-d44043caf4c0.png">
+  </td></tr></table>
+  <table class="spacer"><tbody><tr><td height="50px" style="font-size:50px;line-height:50px;">&#xA0;</td></tr></tbody></table> 
+  <table class="row"><tbody><tr>
+    <th class="p-large small-12 large-12 columns first last"><table><tr><th>
+      <table class="spacer"><tbody><tr><td height="20px" style="font-size:20px;line-height:20px;">&#xA0;</td></tr></tbody></table> 
+      <p mc:edit="">Hey Friend,</p>
+
+      <p mc:repeatable="">Remember the days of Silence=Death? We do too. That's why we're sending a huge delegation of Housing Works advocates
+        to Washington DC this Monday to SPEAK UP, ACT UP, and FIGHT BACK to make sure the healthcare bill - that would leave
+        20 million people without health insurance - goes straight to the circular file.</p>
+
+      <p mc:hideable="">You can help by making a quick donation today to fund this effort.</p>
+    </th>
+<th class="expander"></th></tr></table></th>
+  </tr></tbody></table>
+  <table class="row"><tbody><tr>
+    <th class="small-12 large-12 columns first last"><table><tr><th>
+      <table class="button large expanded"><tr><td><table><tr><td><center data-parsed=""><a href="#" align="center" class="float-center">Support Housing Works</a></center></td></tr></table></td>
+<td class="expander"></td></tr></table>
+    </th>
+<th class="expander"></th></tr></table></th>
+  </tr></tbody></table>
+  <table class="row"><tbody><tr>
+    <th class="small-12 large-12 columns first last"><table><tr><th>
+      <table class="spacer"><tbody><tr><td height="10px" style="font-size:10px;line-height:10px;">&#xA0;</td></tr></tbody></table> 
+      <p mc:edit="">
+        We promised we wouldn't back down, and we're not going to. Check out our action last week that made national news. Just a
+        taste of what Monday will bring!
+      </p>
+      <table class="spacer"><tbody><tr><td height="10px" style="font-size:10px;line-height:10px;">&#xA0;</td></tr></tbody></table> 
+      <img mc:edit="" src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/b3a5d35c-fc87-4901-bb1e-be2e7db2c1cd.png">
+      <table class="spacer"><tbody><tr><td height="40px" style="font-size:40px;line-height:40px;">&#xA0;</td></tr></tbody></table> 
+    </th>
+<th class="expander"></th></tr></table></th>
+  </tr></tbody></table>
+  <table class="row mission"><tbody><tr>
+    <th class="small-12 large-12 columns first last"><table><tr><th>
+      <table class="spacer mission"><tbody><tr><td height="40px" style="font-size:40px;line-height:40px;">&#xA0;</td></tr></tbody></table> 
+      <h4 mc:edit="">Our mission</h4>
+      <p mc:edit="">
+        Housing Works is a healing community of people living with and affected by HIV/AIDS. Our mission is to end the dual crises of homelessness and AIDS through relentless advocacy, the provision of lifesaving services, and entrepreneurial businesses that sustain our efforts.
+      </p>
+      <table class="button secondary"><tr><td><table><tr><td><a href="*|DONATE_URL|*">Donate Now</a></td></tr></table></td></tr></table>
+    </th>
+<th class="expander"></th></tr></table></th>
+  </tr></tbody></table>
+</td></tr></tbody></table>
+
+
+          <table align="center" class="container float-center"><tbody><tr><td>
+            <table class="row footer"><tbody><tr>
+              <th class="small-12 large-8 columns first"><table><tr><th>
+                <table class="spacer show-for-large"><tbody><tr><td height="40px" style="font-size:40px;line-height:40px;">&#xA0;</td></tr></tbody></table> >
+                <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/a6929ac8-16df-496b-8d2c-9022053129dc.png">
+                <table class="spacer"><tbody><tr><td height="15px" style="font-size:15px;line-height:15px;">&#xA0;</td></tr></tbody></table> 
+                <p class="address small">*|HTML:LIST_ADDRESS_HTML|*</p>
+              </th></tr></table></th>
+              <th class="small-12 large-4 columns last"><table><tr><th>
+                <table class="spacer show-for-large"><tbody><tr><td height="50px" style="font-size:50px;line-height:50px;">&#xA0;</td></tr></tbody></table> 
+                <table class="menu"><tr><td><table><tr>
+                  <th class="menu-item btn btn-info float-center"><a href="https://www.facebook.com/sharer/sharer.php?u=" target="_blank">
+                    <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/fc49f95a-ea25-451a-b29d-e81213b9e0e9.png" width="50px" alt="facebook">
+                  </a></th>
+                  <th class="menu-item btn btn-info float-center"><a href="https://twitter.com/intent/tweet?text=" target="_blank">
+                    <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/46a32241-868e-47d8-91e6-2761477fdcc5.png" width="50px" alt="twitter">
+                  </a></th>
+                  <th class="menu-item btn btn-info float-center"><a href="mailto:?subject=&amp;body=" target="_blank">
+                    <img src="https://gallery.mailchimp.com/9a96dbba87ada129104713f59/images/f83869d9-dd62-4c85-86c7-652103e49935.png" width="50px" alt="instagram">
+                  </a></th>
+                </tr></table></td></tr></table>
+              </th></tr></table></th>
+            </tr></tbody></table>
+            <hr class="no-margin no-border-bottom">
+            <table class="row subfooter"><tbody><tr>
+              <th class="small-12 large-12 columns first last"><table><tr><th>
+                <table class="spacer"><tbody><tr><td height="20px" style="font-size:20px;line-height:20px;">&#xA0;</td></tr></tbody></table> 
+                <p class="small">
+                  *|LIST:DESCRIPTION|*
+                </p>
+                <p class="small">
+                  <a href="*|UPDATE_PROFILE|*">Update your email preferences</a> to choose the types of emails you receive.
+                </p>
+                <p class="small">
+                  <a href="*|UNSUBSCRIBE|*">Unsubscribe</a> *|EMAIL|* from this list.
+                </p>
+                <p class="small">
+                  Copyright (C) *|CURRENT_YEAR|* *|LIST:COMPANY|* All rights reserved.
+                </p>
+          
+              </th>
+<th class="expander"></th></tr></table></th>
+            </tr></tbody></table>
+          </td></tr></tbody></table>
+        </center>
+      </td>
+    </tr>
+  </table>
+  <!-- prevent Gmail on iOS font size manipulation -->
+  <div style="display:none; white-space:nowrap; font:15px courier; line-height:0;"> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; </div>
+</body>
+
+</html>
