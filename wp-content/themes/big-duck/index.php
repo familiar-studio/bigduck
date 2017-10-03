2d8781dff846d0bb58c111637b756e7acb7bd30b
Keith Mancuso <keithmancuso@gmail.com>
2017-10-03 18:02:23 -0400

    email remove extra bits

--- wp-content/themes/big-duck/index.php
+++ wp-content/themes/big-duck/index.php
@@ -1,7 +1,2 @@
 <?php require './emails/email-processor.php'; ?>
-
-<!--
-  Build the email as a PHP file and then just import it in here
--->
-
 <?php echo require './wp-content/themes/big-duck/emails/index.html'; ?>
